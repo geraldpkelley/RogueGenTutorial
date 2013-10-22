fdb19beb266aaaeccfa9d841d45a6af8fe9dc728
Gerald Kelley <ilikepopsoda@gmail.com>
2013-10-22 17:54:55 -0500

    Changed room meshes to something better
    Replaced the room meshes (previously plain octagons) with meshes
    resembling hallways and rooms. Looks better now.

--- Assets/Scripts/rogGridSpaceProperties.cs
+++ Assets/Scripts/rogGridSpaceProperties.cs
@@ -5,5 +5,14 @@ public class rogGridSpaceProperties : MonoBehaviour {
 
 	public bool[] openDirNESW = new bool[4];
 	public bool twoWayCorner;
+	public bool fourWay = false;
+	
+	void Start(){
+		if (fourWay == true){
+			Renderer[] rends = GetComponentsInChildren<Renderer>();
+			int n = Random.Range (0, 4);
+			rends[n].enabled = true;
+		}
+	}
 	
 }
