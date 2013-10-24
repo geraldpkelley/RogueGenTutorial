d4f0e5baee237b4d2fa5e4f4f539c35661161c68
Gerald Kelley <ilikepopsoda@gmail.com>
2013-10-23 20:48:01 -0500

    Commented code

--- Assets/Scripts/rogGridSpaceProperties.cs
+++ Assets/Scripts/rogGridSpaceProperties.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class rogGridSpaceProperties : MonoBehaviour {
-
+	
+	// Define which directions are open for this tile in the inspector using openDirNESW.
 	public bool[] openDirNESW = new bool[4];
 	public bool twoWayCorner;
 	public bool fourWay = false;
 	
 	void Start(){
+		// Randomly picks which fourway tile will be displayed.
 		if (fourWay == true){
 			Renderer[] rends = GetComponentsInChildren<Renderer>();
 			int n = Random.Range (0, 4);
--- Assets/Scripts/rogLevelGenerator.cs
+++ Assets/Scripts/rogLevelGenerator.cs
@@ -4,36 +4,39 @@ using System.Linq;
 
 public class rogLevelGenerator : MonoBehaviour {
 	
-	public int gridUnit = 5;
-	public int[] gridMaxXZ = new int[2];
-	public bool[,] vGrid = new bool[50, 50];
-	public int[] curTileXZ = new int[2];
-	public int[] undoMove = new int[2];
-	public int roomsCount;
-	public int maxRooms;
-	public int lineStopNum;
-	public int[] startXZ;
-	int pickDirNESW;
-	int changeDir;
-	bool gridGenDone = false;
 	
+	// Variables for GenerateGrid()
+	public int gridUnit = 25; // How big is a grid space?
+	public int[] gridMaxXZ = new int[2]; // What are the maximum dimensions of the grid?
+	public bool[,] vGrid = new bool[50, 50]; // The virtual grid that contains the basic level layout
+	public int[] curTileXZ = new int[2]; // The current tile being examined
+	public int[] undoMove = new int[2]; // Saves the tile location before curTileXZ moves on, in case it moves somewhere it shouldn't
+	public int roomsCount; // How many spaces have been filled?
+	public int maxRooms; // What's the max spaces to fill?
+	public int currentLineNum; // How long is the current path being created?
+	public int lineStopNum; // How long should a path be before curTileXZ is reset back to the startXZ?
+	public int[] startXZ; // Where does the level generation start?
+	int pickDirNESW; // What direction does curTileXZ go next?
+	int changeDir; 
+	
+	
+	// Variables for FillGrid()
+	// Tile sets organized by how many open sides they have
 	public Transform[] tiles1Way = new Transform[4];
 	public Transform[] tiles2Way = new Transform[6];
 	public Transform[] tiles3Way = new Transform[4];
 	public Transform[] tiles4Way = new Transform[1];
+	
 	static public bool generating;
 	Transform spawnTile;
 	public int[] checkSpaceXZ = new int[2];
-	public bool[] dirCheckResultNESW = new bool[4];
+	public bool[] dirCheckResultNESW = new bool[4]; // Used to determine where surrounding filled grid spaces are
 	public rogGridSpaceProperties gridSpaceProps;
 	public int numDirs = 0;
-	public int testNum = 0;
 	
 	
 	void Start () {
 		roomsCount = 0;
-		//int x = Random.Range (0, gridMaxXZ[0]);
-		//int y = Random.Range (0, gridMaxXZ[1]);
 		startXZ = new int[2]{gridMaxXZ[0]/2, gridMaxXZ[1]/2};
 		vGrid[startXZ[0], startXZ[1]] = true;
 		curTileXZ = new int[2]{startXZ[0], startXZ[1]};
@@ -45,16 +48,16 @@ public class rogLevelGenerator : MonoBehaviour {
 		checkSpaceXZ = new int[2]{0,0};
 		FillGrid();
 	}
-	
+	// GenerateGrid() creates the virtual grid the contains the basic level layout.
 	void GenerateGrid() {
 		while (roomsCount < maxRooms){
-			testNum++;
-			if (testNum > 600)
-				roomsCount = maxRooms;
-			Debug.Log (testNum);
-			if (curTileXZ.SequenceEqual(startXZ))
+			// If curTileXZ == startXZ, start a new path.
+			if (curTileXZ.SequenceEqual(startXZ)){
 				lineStopNum = Random.Range (2, maxRooms + 1);
-			undoMove = new int[2]{curTileXZ[0], curTileXZ[1]};
+				currentLineNum = 0;
+			}
+			undoMove = new int[2]{curTileXZ[0], curTileXZ[1]}; // Save the current tile before curTileXZ moves on
+			// Move curTileXZ in the direction pickDirNESW points
 			if (pickDirNESW == 0)
 				curTileXZ[1]++;
 			if (pickDirNESW == 1)
@@ -63,23 +66,24 @@ public class rogLevelGenerator : MonoBehaviour {
 				curTileXZ[1]--;
 			if (pickDirNESW == 3)
 				curTileXZ[0]--;
+			// If curTileXZ moves outside of the grid, undo that move.
 			if (curTileXZ[0] > gridMaxXZ[0] || curTileXZ[0] < 0 ||
 				curTileXZ[1] > gridMaxXZ[1] || curTileXZ[1] < 0){
 				curTileXZ = new int[2]{undoMove[0], undoMove[1]};
 			} else {
+				// If the new space is empty, fill it, advance the room count and check if the path needs to be reset.
 				if (vGrid[curTileXZ[0], curTileXZ[1]] == false){
 					vGrid[curTileXZ[0], curTileXZ[1]] = true;
 					roomsCount++;
-					
-					// The line stop makes the 
-					if (roomsCount % lineStopNum == 0){
+					currentLineNum++;
+					if (currentLineNum == lineStopNum){
 						curTileXZ = new int[2]{startXZ[0], startXZ[1]};
 					}
 				}
 			}
 			//pickDirNESW = Random.Range (0, 4);  //Simple direction picker. This one makes the generator tend to cluster the rooms.
 			
-		////////  This way makes the generator spread out a little more.  ////////////////
+		////////  This way makes the generator spread out a little more by making it harder for a path to double-back  ////////////////
 			int n = Random.Range (0, 101);
 			if (n < 10)
 				changeDir = 2;
@@ -92,16 +96,16 @@ public class rogLevelGenerator : MonoBehaviour {
 				pickDirNESW = 0;
 			if (pickDirNESW < 0)
 				pickDirNESW = 3;
-		//////////////////////////////////////////////////////////////////////////////////
-			
-			if (roomsCount == maxRooms)
-				gridGenDone = true;
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		}
 	}
 	
+	// FillGrid() looks at the newly filled vGrid[] and plops in prefabs that form the overall level design.
 	void FillGrid(){
 		while (generating == true){
-			//if the space checker gets to the end of a line, return. If the return brings you beyond the Y max, end 
+			//if the space checker gets to the end of a line, return. If the return brings you beyond the Z max, end
+			// This makes it so we start at the lower left corner of the grid and check every space of the grid for fills until we get to the top right.
 			if (checkSpaceXZ[0] > gridMaxXZ[0]){
 				checkSpaceXZ[1]++;
 				if (checkSpaceXZ[1] > gridMaxXZ[1]){
@@ -110,11 +114,11 @@ public class rogLevelGenerator : MonoBehaviour {
 				checkSpaceXZ[0] = 0;
 			} else {
 				if (vGrid[checkSpaceXZ[0], checkSpaceXZ[1]] == true){
+					// CheckDirs() looks at the surrounding spaces and determines how many connections this tile has and in what direction.
 					CheckDirs (checkSpaceXZ[0], checkSpaceXZ[1]);
 					if (numDirs == 1){
 						//You have the number of open directions and which directions those are. Now cycle through the different spawners and see which one fits
 						for (int i = 0; i < tiles1Way.Length; i++){
-							testNum = i;
 							gridSpaceProps = tiles1Way[i].GetComponent<rogGridSpaceProperties>();
 							if (gridSpaceProps.openDirNESW.SequenceEqual (dirCheckResultNESW)){
 								Instantiate (tiles1Way[i], new Vector3(checkSpaceXZ[0] * gridUnit, 0, checkSpaceXZ[1] * gridUnit), tiles1Way[i].rotation);
@@ -144,7 +148,7 @@ public class rogLevelGenerator : MonoBehaviour {
 						Instantiate (tiles4Way[0], new Vector3(checkSpaceXZ[0] * gridUnit, 0, checkSpaceXZ[1] * gridUnit), tiles4Way[0].rotation);
 					}
 				}
-				checkSpaceXZ[0]++;
+				checkSpaceXZ[0]++; // Advance one space down the row
 			}
 		}
 	}
